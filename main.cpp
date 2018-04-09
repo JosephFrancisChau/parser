8d6daa82a8967202c447130592952cc0170ff3b4
wendyji131 <31259905+wendyji131@users.noreply.github.com>
2018-04-09 11:15:47 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,27 +6,28 @@ using namespace std;
 vector<string> allWords;
 string outputFile;
 Token start;
+unsigned index;
 //fstream coutfile(outputFile);
 
 int main(int argc, const char * argv[]) {
-    string inputFile, word, ofile;
-    //ifstream sourceFile;
-    
-    
-    //test sample source code
-    cout << "Enter test file name(format: filename.txt): ";
-    cin >> inputFile;
-    
-    cout << "Enter test output file name(format: filename.txt): ";
-    cin >> outputFile;
-    
-    fstream coutfile;
-    coutfile.open(outputFile, fstream::out);
-    
-    ifstream sourceFile(inputFile);
-    
-    if (sourceFile.is_open()) {
-                // call splitWord can creat allWords
+	string inputFile, word, ofile;
+	//ifstream sourceFile;
+
+
+	//test sample source code
+	cout << "Enter test file name(format: filename.txt): ";
+	cin >> inputFile;
+
+	cout << "Enter test output file name(format: filename.txt): ";
+	cin >> outputFile;
+
+	fstream coutfile;
+	coutfile.open(outputFile, fstream::out);
+
+	ifstream sourceFile(inputFile);
+
+	if (sourceFile.is_open()) {
+		// call splitWord can creat allWords
 		while (sourceFile >> word) {
 			vector<string> vec = splitWord(word);
 			for (unsigned i = 0; i < vec.size(); ++i) {
@@ -36,18 +37,21 @@ int main(int argc, const char * argv[]) {
 
 		//test the new code on allWords works or nor
 		for (unsigned i = 0; i < allWords.size(); ++i) {
-			Token token = lexer(allWords.at(i));
-			cout << token.type << "\t\t" << token.value << endl;
-			coutfile << token.type << "\t\t" << token.value << endl;
+			Token start = lexer(allWords.at(i));
+			cout << start.type << "\t\t" << start.value << endl;
+			coutfile << start.type << "\t\t" << start.value << endl;
+			index = i;
+			Parser(start, index);
 		}	
-    }
-    else {
-        cout << "Can't open the file: " << inputFile << endl;
-    }
-    
-    sourceFile.close();
-    
-    
-    return 0;
-}
 
+
+	}
+	else {
+		cout << "Can't open the file: " << inputFile << endl;
+	}
+
+	sourceFile.close();
+
+
+	return 0;
+}
